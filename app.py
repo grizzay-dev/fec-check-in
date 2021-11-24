674031f51a4473b483cef3365d63bc82329481ea
Harry Davis <harrydavis.dev@gmail.com>
2021-11-24 18:41:40 +1000

    started bridging existing functions to use sqlite3 db. Still need to update templates to use db data.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-#from re import template
+import re
+import sqlite3
+from sqlite3 import Error
 from flask import Flask, request, redirect, render_template, url_for
 from flask.scaffold import F
 from flask.wrappers import Response
@@ -18,6 +20,9 @@ DATA_PATH = r"data/users.json"
 UPLOAD_FOLDER = r'static/images/users'
 ALLOWED_EXTENSIONS = {'png'}
 
+#STATIC VARS2
+DB_PATH = r'data/data.db'
+
 #WEB SERVER
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -28,11 +33,49 @@ def getUserData(id):
         tmp = json.load(data)
         user = tmp[id]
         return user
+#for db
+def get_user(id, db):
+    conn = None
+    try:
+        conn = sqlite3.connect(db)
+        c = conn.cursor()
+        q_find_user_by_id = "SELECT * FROM user WHERE id='{0}'".format(id)
+        c.execute(q_find_user_by_id)
+        rows = c.fetchall()
+
+    except Error as e:
+        print(e)
+
+    finally:
+        if conn:
+            print(rows)
+            conn.close()
+            return rows
+            
 
 def getUsers():
     with open(DATA_PATH, 'r') as data:
         tmp = json.load(data)
         return tmp
+
+#for db
+def get_all_users(db):
+    conn = None
+    try:
+        conn = sqlite3.connect(db)
+        c = conn.cursor()
+        q_find_users = "SELECT * FROM user"
+        c.execute(q_find_users)
+        rows = c.fetchall()
+
+    except Error as e:
+        print(e)
+
+    finally:
+        if conn:
+            print(rows)
+            conn.close()
+            return rows
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -45,6 +88,8 @@ def allowed_file(filename):
 @app.route('/')
 def root():
     data = getUsers()
+    data_db = get_all_users(DB_PATH)
+
     return render_template("status.html", title="STATUS BOARD", users=data)
 
 @app.route('/old')
@@ -61,6 +106,7 @@ def createProfile():
         req =request.form
         newUser = user.User(req["fName"], req["lName"], req["email"])
         user.createNew(newUser)
+        user.create_user(newUser, DB_PATH)
         return redirect("/")
 
 #USER PROFILE - View / edit user profiles.
@@ -69,6 +115,7 @@ def profile(id):
     if request.method=='GET':
         u = urlparse(request.base_url)
         profile = getUserData(id)
+        profile = get_user(id, DB_PATH)
         profileTitle = "Profile  - {0} {1}".format(profile['fName'], profile['lName'])
         return render_template("profile.html", title=profileTitle, user=profile, id = id, statusOptions=user.STATUS, hostname = u.hostname)
     
@@ -77,13 +124,13 @@ def profile(id):
         userUpdate = {
                 'status': req['status'],
                 "fName": req['fName'],
-                
                 'lName': req['lName'],
                 'email': req['email'],
                 'comment': req['comment'],
                 'lastmod': user.getTimeStamp()
             }
         user.updateByID(id, userUpdate)
+        user.update_user(id, userUpdate, DB_PATH)
         if 'profilePicture' in request.files:
             file = request.files['profilePicture']
             if file and allowed_file(file.filename ):
@@ -97,6 +144,7 @@ def profile(id):
 def delete(id):
     if request.method=='POST':
         user.deleteUser(id)
+        user.delete_user(id, DB_PATH)
         return redirect("/")
 
 #UPDATE STATUS - Allows individual users to update their status.
@@ -113,6 +161,7 @@ def updateStatusGet(id):
         profile['comment'] = req['comment']
         profile['lastmod'] = user.getTimeStamp()
         user.updateByID(id, profile)
+        user.update_user(id, user, DATA_PATH)
         return redirect("/")
     
 #READ ONLY
@@ -138,8 +187,8 @@ def logs():
 ###                                   END - ROUTES                                     ###
 ###------------------------------------------------------------------------------------###
 
-#if __name__ == '__main__':
-    #serve(app, host='0.0.0.0', port=80)
+if __name__ == '__main__':
+    serve(app, host='0.0.0.0', port=80)
     
 
 
--- user.py
+++ user.py
@@ -5,6 +5,8 @@ import os.path
 import json
 from datetime import datetime, tzinfo
 import logging
+import sqlite3
+from sqlite3 import Error
 
 #LOGGING
 logger = logging.getLogger("audit_log")
@@ -43,6 +45,23 @@ def updateByID(id, user):
             json.dump(tmp, d, sort_keys=True, indent=4, separators=(',', ': '))
             d.close()
 
+#update for db
+def update_user(id, user, db):
+    conn = None
+    try:
+        conn = sqlite3.connect(db)
+        c = conn.cursor()
+        q_update_user = "UPDATE user SET email='{0}', fname='{1}', lname='{2}', lastmod='{3}', status='{4}', comment='{5}' WHERE id='{6}'".format(user['email'], user['fName'], user['lName'], user['lastmod'], user['status'], user['comment'], id)
+        c.execute(q_update_user)
+        conn.commit()
+
+    except Error as e:
+        print(e)
+
+    finally:
+        if conn:
+            conn.close()
+
 def createNew(user):
         with open(data_path, "r") as d:
             try:
@@ -62,6 +81,40 @@ def createNew(user):
         with open(data_path, "r+") as d:
             json.dump(tmp, d, sort_keys=True, indent=4, separators=(',', ': '))
             d.close()
+
+#new for db
+def create_user(user, db):
+    conn = None
+    try:
+        conn = sqlite3.connect(db)
+        c = conn.cursor()
+        q_new_user = "INSERT INTO user (id, email, fname, lname, lastmod, status, comment) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}')".format(user.id, user.email, user.fName, user.lName, user.lastmod, user.status, "new user")
+        c.execute(q_new_user)
+        conn.commit()
+
+    except Error as e:
+        print(e)
+
+    finally:
+        if conn:
+            conn.close()
+
+#delete for db
+def delete_user(id, db):
+    conn = None
+    try:
+        conn = sqlite3.connect(db)
+        c = conn.cursor()
+        q_del_user = "DELETE FROM user WHERE id='{0}'".format(id)
+        c.execute(q_del_user)
+        conn.commit()
+
+    except Error as e:
+        print(e)
+
+    finally:
+        if conn:
+            conn.close()
 
 def deleteUser(id):
     with open(data_path, "r") as d:
